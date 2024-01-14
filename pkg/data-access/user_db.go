a57141b8e4a0ac46ee8f739c903781dd5fc59f55
ndt94 <ndt94hvnh@gmail.com>
2024-01-14 22:30:51 +0700

    update and get user api part 2

--- pkg/data-access/user_db.go
+++ pkg/data-access/user_db.go
@@ -29,6 +29,32 @@ func (s *userDA) GetUserByUserName(c context.Context, username string) (dto.User
 	}, err
 }
 
+func (s *userDA) Get(c context.Context, ID int) (dto.User, error) {
+	user := dto.User{ID: ID}
+
+	if err := s.dbc.NewSelect().
+		Model(&user).
+		Relation("Teaches").
+		Relation("Studies").
+		WherePK().
+		Scan(c); err != nil {
+		return dto.User{}, err
+	}
+
+	return user, nil
+}
+
+func (s *userDA) Update(c context.Context, id int, user dto.User) (dto.User, error) {
+	if _, err := s.dbc.NewUpdate().
+		Model(&user).
+		Where("id = ?", id).
+		Returning("*").
+		Exec(c); err != nil {
+		return dto.User{}, err
+	}
+	return user, nil
+}
+
 // NewStudentDA creates a new Student Data Access
 func NewUserDA(dbc *bun.DB) *userDA {
 	return &userDA{dbc}
